9e9931cb36065a63ebc61ee6f7624d6de57074fe
Jason Ellis <palamedes@rocketmail.com>
2020-07-22 14:04:17 -0400

    updating the desktop controller to know how to deal with pages.   .json loading not in yet clearly.

--- app/controllers/desktop_controller.rb
+++ app/controllers/desktop_controller.rb
@@ -1,7 +1,17 @@
 class DesktopController < ApplicationController
 
   # [GET] /
+  # Load the desktop
   def index
+  end
+
+  # [GET] /:page[.json]
+  # @TODO This will need to either work as a full page load, or a json pull of just the document
+  def page
+    document = MarkdownParser::Parser.parse "page/#{params[:page]}"
+    redirect_to "/" if document[:vars][:published] == false
+
+    @html = document[:html].html_safe
   end
 
 end
