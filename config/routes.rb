646aa1d7e2c70d6531f4cdb3ca00db3475ae920a
Jason Ellis <palamedes@rocketmail.com>
2020-07-22 13:13:04 -0400

    Updated routes to add wildcard pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Wildcard Page route which will show desktop and window
+  get '/:page', to: "desktop#document"
+
+  # Default index route
   root 'desktop#index'
 
 end
