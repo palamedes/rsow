e03c3fbde295c74b09a0a0c87e8689d7378d0944
Jason Ellis <palamedes@rocketmail.com>
2020-07-22 13:13:25 -0400

    Method name update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Wildcard Page route which will show desktop and window
-  get '/:page', to: "desktop#document"
+  get '/:page', to: "desktop#page"
 
   # Default index route
   root 'desktop#index'
