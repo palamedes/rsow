382eaded227e433fba5d4b0a5a72cb9556bd723c
Jason Ellis <palamedes@rocketmail.com>
2020-07-22 21:43:48 -0400

    Adding more css, js and html to make the window frame work look correct and act ... better.. its got a ways to go

--- app/javascript/packs/desktop.js
+++ app/javascript/packs/desktop.js
@@ -6,14 +6,22 @@ $(document).on('ready turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
 
   // Windows
-  $('.ui.window').draggable({ handle: '.headbar' });
+  $('.ui.window').draggable({ 
+    handle: '.headbar',
+    containment: 'body'
+  });
   $('.ui.window').resizable(
     {
-      animate: true,
       containment: 'body',
       helper: "window-resizer",
       minHeight: 200,
-      minWidth: 300,
+      minWidth: 400,
+      stop: function(resize, ui) {
+        var newHeight = ui.size.height - 16;
+        $(ui.element[0]).children('div.ui.compact.segment').height(newHeight);
+        var menuHeight = $(ui.element[0]).find('div.ui.top.attached.menu').first().outerHeight();
+        $(ui.element[0]).children('div.ui.compact.segment').children('div.ui.bottom.attached.segment').height(newHeight - menuHeight - 28);
+      }
     }
   );
 
