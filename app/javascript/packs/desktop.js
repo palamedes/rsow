af90bb4efed485b7b7e3e968aebf102036985491
Jason Ellis <palamedes@rocketmail.com>
2020-07-23 16:30:45 -0400

    As good as any point to check everything and push

--- app/javascript/packs/desktop.js
+++ app/javascript/packs/desktop.js
@@ -1,29 +1,55 @@
 
-
 $(document).on('ready turbolinks:load', function() {
 
   // Turn on all dropdowns.
   $('.ui.dropdown').dropdown();
 
-  // Windows
-  $('.ui.window').draggable({ 
-    handle: '.headbar',
-    containment: 'body'
-  });
-  $('.ui.window').resizable(
-    {
-      containment: 'body',
-      helper: "window-resizer",
-      minHeight: 200,
-      minWidth: 400,
-      stop: function(resize, ui) {
-        var newHeight = ui.size.height - 16;
-        $(ui.element[0]).children('div.ui.compact.segment').height(newHeight);
-        var menuHeight = $(ui.element[0]).find('div.ui.top.attached.menu').first().outerHeight();
-        $(ui.element[0]).children('div.ui.compact.segment').children('div.ui.bottom.attached.segment').height(newHeight - menuHeight - 28);
-      }
+  $('div.ui.window').height($('body').height());
+
+  var resizableWindowArguments = {
+    containment: '.desktop',
+    minHeight: 200,
+    minWidth: 400,
+    handles: 'se',
+    resize: function(resize, ui) {
+      adjustInternalWindowHeight($(ui.element[0]), ui.size.height);
+    },
+    stop: function(resize, ui) {
+      adjustInternalWindowHeight($(ui.element[0]), ui.size.height);
     }
-  );
+  }
 
 
+  // Windows
+  $('div.ui.window').draggable({
+    handle: '.headbar',
+    containment: '.desktop'
+  });
+  $('div.ui.window').resizable(resizableWindowArguments);
+
+  // After resizing window, resize the contents a tad.
+  var adjustInternalWindowHeight = function($dis, height) {
+    var newHeight = height - 11;
+    $dis.children('div.ui.compact.segment').height(newHeight);
+    var menuHeight = $dis.find('div.ui.top.attached.menu').first().outerHeight();
+    $dis.find('div.ui.bottom.attached.segment').height(newHeight - menuHeight - 36);
+  }
+  // Set WindowSize
+  var setWindowSize = function($window, height, width) {
+    $window.height(height);
+    $window.width(width);
+    adjustInternalWindowHeight($window, height);
+  }
+
+  $('button.restore.window').click(function() {
+    restoreWindow($(this).parents('div.ui.window'));
+  });
+
+  var restoreWindow = function($window) {
+    setWindowSize($window, 500,800);
+//    $('div.ui.window').resizable(resizableWindowArguments)
+  }
+
+
+  
 });
