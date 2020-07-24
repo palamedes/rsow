58b7abd281902981f4f3a70920c435acbcd9b3e1
Jason Ellis <palamedes@rocketmail.com>
2020-07-23 22:18:14 -0400

    For now .. still need better way to get this info into the template

--- app/controllers/desktop_controller.rb
+++ app/controllers/desktop_controller.rb
@@ -9,11 +9,15 @@ class DesktopController < ApplicationController
   # @TODO This will need to either work as a full page load, or a json pull of just the document
   def page
     document = MarkdownParser::Parser.parse "page/#{params[:page]}"
-    redirect_to "/" if document[:vars][:published] == false
 
-    @windowSlug = document[:vars][:slug].html_safe
-    @windowTitle = document[:vars][:title].html_safe
-    @windowContent = document[:html].html_safe
+    if document[:vars][:published] == false
+      redirect_to "/"
+    else
+      @windowSlug = document[:vars][:slug].html_safe rescue nil
+      @windowTitle = document[:vars][:title].html_safe
+      @windowContent = document[:html].html_safe
+    end
+
   end
 
 end
