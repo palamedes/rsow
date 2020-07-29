1a2ebe2d4c793c4971298090b9b580139b3921d8
Jason Ellis <palamedes@rocketmail.com>
2020-07-28 20:37:20 -0400

    Company update..

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,23 @@
 class Company < ApplicationRecord
 
+  # Relationships
+  has_many :valuations
+
   # Validations
-  validates :symbol, :asset_type, :name, :description, :exchange, :currency, :country, presence: true
+  validates :symbol, :assettype, :name, :description, :exchange, :currency, :country, presence: true
+
+  # When doing a .from_json {json} we have to do some special massaging of a couple fo the keys.
+  def attributes=(hash)
+    hash.each do |key, value|
+      # Massage the key a tad
+      key = key.downcase
+      key = 'weekhigh52' if key == '52weekhigh'
+      key = 'weeklow52' if key == '52weeklow'
+      key = 'daymovingaverage50' if key == '50daymovingaverage'
+      key = 'daymovingaverage200' if key == '200daymovingaverage'
+      # Send all those key:value pairs to this object before saving it
+      send("#{key}=", value)
+    end
+  end
 
 end
