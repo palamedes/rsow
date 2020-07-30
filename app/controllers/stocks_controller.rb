22c57bffd05705ae06a7281ff433b4d4e25657ed
Jason Ellis <palamedes@rocketmail.com>
2020-07-30 12:03:08 -0400

    add list of companies

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,10 +3,12 @@ class StocksController < ApplicationController
   # [GET] /stocks[.json]
   # Load the desktop with the stocks window and the stocks in the system we are currently watching + positions
   def index
+    # Get a list of our companies for the view.
+    @companies = Company.all
+
     respond_to do |format|
       format.json { render json: {} }
       format.html {
-
       }
     end
   end
