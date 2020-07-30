072df8e75cf9b20f136537bb2f9eda441fa85bc8
Jason Ellis <palamedes@rocketmail.com>
2020-07-30 18:28:19 -0400

    Updated the valuation to_json (as_json) to output only that which we need to charting.

--- app/models/valuation.rb
+++ app/models/valuation.rb
@@ -6,4 +6,11 @@ class Valuation < ApplicationRecord
   # validations
   validates :datetime, :open, :high, :low, :close, :volume, presence: true
 
+  # Dump to json just what we need
+  def as_json(*)
+    super.except('id', 'company_id', 'datetime', 'created_at', 'updated_at').tap do |hash|
+      hash['date'] = datetime
+    end
+  end
+
 end
