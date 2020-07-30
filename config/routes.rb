0e02389cf78a127dca04d2f0c2cbfd47f64a49ed
Jason Ellis <palamedes@rocketmail.com>
2020-07-30 10:40:48 -0400

    Adding stock resource to routes file Adding as to pages and blog posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Wildcard Page route which will show desktop and window
-  get '/:page', to: "desktop#page"
-  get '/blog/:post', to: "desktop#post"
+  get '/:page', to: "desktop#page", as: :page
+  get '/blog/:post', to: "desktop#post", as: :post
+
+  resources :stocks, only: [ :index, :show ]
 
   # Default index route
   root 'desktop#index'
