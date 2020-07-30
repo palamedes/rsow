5ceb9a8663ceb4cf93e3d53e997e7c984336d6b2
Jason Ellis <palamedes@rocketmail.com>
2020-07-30 11:15:44 -0400

    Woops.. Wildcard routes must go at the bottom in a specific order.. heh..

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Wildcard Page route which will show desktop and window
-  get '/:page', to: "desktop#page", as: :page
-  get '/blog/:post', to: "desktop#post", as: :post
-
+  # Stocks Resource
   resources :stocks, only: [ :index, :show ]
+
+  # Wildcard Page route which will show desktop and window
+  get '/blog/:post', to: "desktop#post", as: :post
+  get '/:page', to: "desktop#page", as: :page
 
   # Default index route
   root 'desktop#index'
