a9ad936f06e9d9f72e5898d2c2094dca91a636ce
Jason Ellis <palamedes@rocketmail.com>
2020-08-01 15:32:16 -0400

    updating the alphavantage api module to know how ot get quotes.. this will likely be how I do this going forward..

--- lib/alphavantage.rb
+++ lib/alphavantage.rb
@@ -7,16 +7,18 @@
 # Example of how to pull Company data using API.
 # https://www.alphavantage.co/query?function=OVERVIEW&symbol=ccl&apikey={apikey}
 
-module Alphavantage
+module AlphavantageAPI
 
   # Get the information for a given company and stuff it into the db.
-  class CompanyAPI
-    # CALL: AlphavantageAPI::Company 'IBM'
+  class Stock
+    attr_reader :response
+
+    # CALL: obj = AlphavantageAPI::Company.new :IBM
     def initialize symbol
       # @TODO MOVE THIS TO ENVIRONMENT OR SOMETHING (For now its not important because its not paid for but a free key)
       alphavantage_apikey = "8M1NABRQBD3I1PEQ"
       # Upcase that symbol just incase.
-      symbol = symbol.upcase
+      symbol = symbol.to_s.upcase
       # Build our URL around what we are getting /// @TODO MOVE KEY OUT OF HERE
       url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{symbol}&apikey=#{alphavantage_apikey}"
       # Go get that data
@@ -28,15 +30,49 @@ module Alphavantage
       # Save that bad boy.
       @company.save
     end
-    # Just incase you need it.
-    def response
-      @response
-    end
+
   end
+
+  # Get a single quote for a given stock
+  class Quote
+    attr_reader :response, :price, :open, :high, :low, :close, :volume, :change, :change_percent
+
+    # CALL: obj = AlphavantageAPI::Quote.new :IBM
+    def initialize sym
+      # Upcase and to string so we can send string or symbol
+      sym = sym.to_s.upcase
+      # Store it
+      @symbol = sym
+      # @TODO MOVE THIS TO ENVIRONMENT OR SOMETHING (For now its not important because its not paid for but a free key)
+      alphavantage_apikey = "8M1NABRQBD3I1PEQ"
+      # define our URL
+      url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{@symbol}&apikey=#{alphavantage_apikey}"
+      # Get a response
+      @response = Faraday.get url
+      # Go through the response and pass out the data
+      # @TODO Add error checking on the response..
+      @price = JSON.parse(@response.as_json['body'])['Global Quote']['05. price'].to_f
+      @open = JSON.parse(@response.as_json['body'])['Global Quote']['02. open'].to_f
+      @high = JSON.parse(@response.as_json['body'])['Global Quote']['03. high'].to_f
+      @low = JSON.parse(@response.as_json['body'])['Global Quote']['04. low'].to_f
+      @volume = JSON.parse(@response.as_json['body'])['Global Quote']['06. volume'].to_f
+      @change = JSON.parse(@response.as_json['body'])['Global Quote']['09. change'].to_f
+      @change_percent = JSON.parse(@response.as_json['body'])['Global Quote']['10. change percent'].to_f
+      @close = JSON.parse(@response.as_json['body'])['Global Quote']['08. previous close'].to_f
+    end
+
+  end
+
+# @TODO make a class that is just a singelton that will do the work of calling quotes over and over for each one of the
+# companies that we have in the system.
+
+
 
   # Get the intraday information for a given company at a 1 minute time scale
   # NOTE:  Okay I'm starting to realize that this isn't useful.  It appears to be delayed by a full day..  bleh.
-  class IntradayAPI
+  class Intraday
+    attr_reader :response, :timeSeries
+
     # AlphavantageAPI::Intraday.get 'IBM'
     def initialize symbol
       # Upcase the symbol
@@ -61,14 +97,6 @@ module Alphavantage
       # Now lets just get the time series
       @timeSeries = @jsonResponse['Time Series (1min)']
       # Now iterate through the time series arrays and dig out the datetime and values
-    end
-    # Give us the response object
-    def response
-      @response
-    end
-    # Get us the timeSeries
-    def timeSeries
-      @timeSeries
     end
     # Save timeSeries information as valuations
     def parseResponse
