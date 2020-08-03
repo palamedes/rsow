5ec380520a2e3fc6cecf58e1634ca81a3c2672e1
Jason Ellis <palamedes@rocketmail.com>
2020-08-03 13:12:08 -0400

    Lets make this more simple for now..

--- db/migrate/20200728193451_create_valuations.rb
+++ db/migrate/20200728193451_create_valuations.rb
@@ -6,16 +6,8 @@ class CreateValuations < ActiveRecord::Migration[6.0]
 
       t.datetime :datetime,     null: false
 
-      t.string :data_type,      null: false, default: :quote
       t.float :price
-
-      t.float :open,            null: false
-      t.float :high,            null: false
-      t.float :low,             null: false
-      t.float :close,           null: false
       t.float :volume,          null: false
-      t.float :change
-      t.float :change_percent
 
       t.timestamps
     end
