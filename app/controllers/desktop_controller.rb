e8cb16390cebfb457421a7a5e094e3cb4a230114
Jason Ellis <palamedes@rocketmail.com>
2020-08-06 12:19:41 -0400

    Page now knows how to render the window as a json string.. this isn't the best way to do this for sure.. but for now until i get a dynamic window creation tool in the UI this will have to do.

--- app/controllers/desktop_controller.rb
+++ app/controllers/desktop_controller.rb
@@ -8,17 +8,20 @@ class DesktopController < ApplicationController
   # [GET] /:page[.json]
   # @TODO This will need to either work as a full page load, or a json pull of just the document
   def page
+
     # Okay attempt to parse the page md file based on the params
-    document = MarkdownParser::Parser.parse "page/#{params[:page]}"
+    @document = MarkdownParser::Parser.parse "page/#{params[:page]}"
     # If it's a document then cool, if not route back to /
-    if document.nil? || document[:published] == false
+    if @document.nil? || @document[:published] == false
       redirect_to "/"
     else
+
       respond_to do |format|
-        format.json { render json: {} }
-        format.html {
-          @document = document
+        format.json {
+          html = render_to_string action: :page, locals: { page: params[:page] }, layout: false, formats: [:html]
+          render json: { html: html }
         }
+        format.html {}
       end
     end
   end
