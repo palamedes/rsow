c912910be502381ab9b7ca20f6fc98edc67e8eaf
Jason Ellis <palamedes@rocketmail.com>
2020-08-24 11:53:25 -0400

    Slug can now be the file name correctly formatted in the event a slug isn't passed.

--- lib/document.rb
+++ lib/document.rb
@@ -1,9 +1,10 @@
 # Class document is the object returned by the parser
 class Document
+  attr_writer   :slug
   attr_accessor :has_variables, :file_name,
                 :published, :sitemap, :title, :excerpt, :layout, :tags, :categories, :image,
                 :gallery, :ribbon, :document_class, :private, :allow_comments, :duration,
-                :costs, :slug, :date, :icon, :audio, :audioautoplay, :audioloop, :audiovolume,
+                :costs, :date, :icon, :audio, :audioautoplay, :audioloop, :audiovolume,
                 :html
 
   # Does this document have any audio?
@@ -15,6 +16,11 @@ class Document
   # @TOOD make it a FULL href?
   def href
     slug.gsub 'blog-', 'blog/'
+  end
+
+  # Return the file name if the slug isn't set
+  def slug
+    (@slug.nil? || @slug.empty?) ? @file_name.gsub(/^post\//, 'blog-').gsub(/\.md$/, '') : @slug
   end
 
 end
