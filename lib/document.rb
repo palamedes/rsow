95506f17eec3898e0b6c6ba63fbef3efbbc028a1
Jason Ellis <palamedes@rocketmail.com>
2020-08-24 11:36:23 -0400

    Adding file name to the document so it knows more about itself

--- lib/document.rb
+++ lib/document.rb
@@ -1,9 +1,10 @@
 # Class document is the object returned by the parser
 class Document
-  attr_accessor :published, :sitemap, :title, :excerpt, :layout, :tags, :categories, :image,
+  attr_accessor :has_variables, :file_name,
+                :published, :sitemap, :title, :excerpt, :layout, :tags, :categories, :image,
                 :gallery, :ribbon, :document_class, :private, :allow_comments, :duration,
                 :costs, :slug, :date, :icon, :audio, :audioautoplay, :audioloop, :audiovolume,
-                :html, :has_variables
+                :html
 
   # Does this document have any audio?
   def has_audio?
