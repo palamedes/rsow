aa332e1bcc49b217348ac583accfc363689d1b58
Jason Ellis <palamedes@rocketmail.com>
2020-09-04 14:00:09 -0400

    Updating the bot to work by pulling data from yahoo..

--- lib/stockbot.rb
+++ lib/stockbot.rb
@@ -8,6 +8,36 @@ class StockBot
     Company.all.map{|c| c.symbol}
   end
 
+  def self.getBots
+    Company.all.map{|c| StockBot.new c.symbol}
+  end
+
+  def self.go
+    looping = true
+    bots = self.getBots
+    while looping == true
+      bots.each do |bot|
+        bot.getQuote rescue "NO GET QUOTE"
+        bot.saveQuote rescue "NO SAVE QUOTE"
+        sleep 5
+      end
+      # Sleep for just over a minute and go again.
+      sleep 30
+    end
+  end
+
+  # Get a company based on the symbol from the DB, OR pull it from API and save it to db
+  def self.getCompany symbol
+    # @TODO REplace this with IEX instead of alphavantage
+    stock = AlphavantageAPI::Stock.new symbol
+    # Load our company or create a new one
+    company = Company.where(symbol: symbol).first_or_create
+    # Stuff the information into the company
+    company.from_json stock.response.body
+    # Save that bad boy.
+    company.save
+  end
+
   # @TODO Each stock will have it's own bot... So this will be an object with ONE company
   #  - Each time a bot looks at the price it iwll have indicators and think about what to do
   #  - We need to know the original stock price but also the buy in price, adn thenthe current price..etc..
@@ -20,53 +50,48 @@ class StockBot
     # Get our company
     getCompany
     # @TODO Test to make sure we are ready to go, else set some error code and don't proceed or let calls happen
-    @client = IEX::Api::Client.new
+#    @client = IEX::Api::Client.new
   end
 
-  # Get a company based on the symbol from the DB, OR pull it from API and save it to db
+  # Get our company data from the db and save it local to the bot
   def getCompany
-    # @TODO REplace this with IEX instead of alphavantage
-    stock = AlphavantageAPI::Stock.new @symbol
-    # Load our company or create a new one
     @company = Company.where(symbol: @symbol).first_or_create
-    # Stuff the information into the company
-    @company.from_json stock.response.body
-    # Save that bad boy.
-    @company.save
+    raise if @company.new_record?
   end
 
   # Get a quote for a symbol and stuff it into the db
   def getQuote
-    # Get our quote
-    @quote = @client.quote @symbol
-    # Save our previous quote
-    @prevQuote = @quote
+    # Make Yahoo Faraday Request
+    req = Faraday.get("https://search.yahoo.com/search?p=#{@symbol}") rescue nil
+    if req.nil? || req.body.nil?
+      puts "Faraday Request threwup"
+      puts req.inspect
+    else
+      quote = req.body.match(/"fin_quotePrice.*?>(.*?)</)
+      if quote[1].nil? || quote[1].empty?
+        puts "Quote returned nil/empty:"
+        puts quote.inspect
+        puts req.inspect
+      else
+        @quote = quote[1].to_f
+        # Save our previous quote
+        @prevQuote = @quote
+
+        puts "#{@symbol}: #{@quote}"
+
+        return @quote
+      end
+    end
+    return nil
   end
 
   # Save that sucker
   def saveQuote
+    return if @quote.nil?
     Valuation.create company: company,
-                     price: @quote.latest_price,
-                     volume: @quote.iex_volume,
+                     price: @quote,
+                     volume: 0,
                      datetime: DateTime.now
   end
-
-  # Do the loop thing for this stock.
-  def go
-    looping = true
-    while looping == true
-      # Get the quote
-      getQuote
-      # save the quote
-      saveQuote
-      # Do some logic around the investment shit... eventually
-      # Sleep for just over a minute and go again.
-      sleep 10
-    end
-  end
-
-
-
-
 
 end
