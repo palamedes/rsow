f57d3acd9892a1340fea58a1569274580f9d106d
Jason Ellis <palamedes@rocketmail.com>
2020-09-21 11:10:49 -0400

    Companies know how to normalize their own data .. sorta.. not complete but it's a start.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,8 @@
 class Company < ApplicationRecord
 
   # Relationships
-  has_many :valuations, -> { order(datetime: :asc) }
+  has_many :quotes, -> { order(datetime: :asc) }
+  has_many :rawquotes, -> { order(datetime: :asc) }
 
   # Validations
   validates :symbol, :assettype, :name, :description, :exchange, :currency, :country, presence: true
@@ -23,6 +24,60 @@ class Company < ApplicationRecord
       # Send all those key:value pairs to this object before saving it
       send("#{key}=", value)
     end
+  end
+
+  # Get company information
+
+  # Get a quote
+
+  # Normalize data
+  # Goes back in time and takes raw quotes and normalizes the data into something we can better use
+  # arguments:
+  #  range = number of days back in time we are going to go to normalize data. Default: 3
+
+  def normalize range = 3
+    return unless range.integer?
+    # Go back in time [range] number of days and do this thing
+    range.downto(0).each do |i|
+      # Get our day
+      day = DateTime.now.midnight - i.days
+      # Get the quotes for the day in question
+      daysQuotes = rawquotes.where(datetime: day.midnight..day.end_of_day).order(:datetime)
+
+      # Do we even have this day?
+      unless daysQuotes.empty?
+        quote = quotes.where(datetime: day.midnight, scale: 'DAY').first_or_create
+        quote.high, quote.low, quote.open, quote.close = periodCandle daysQuotes
+        quote.save
+
+        # Get HOUR range of data
+        (9..16).each do |h|
+          hour = DateTime.now.midnight - i.days + h.hours
+          hourQuotes = rawquotes.where(datetime: hour..hour+1.hour).order(:datetime)
+
+          unless hourQuotes.empty?
+            quote = quotes.where(datetime: hour, scale: 'HOUR').first_or_create
+            quote.high, quote.low, quote.open, quote.close = periodCandle daysQuotes
+            quote.save
+          end
+
+
+        end
+
+
+        # Get 30min range of data
+        # Get 5min range of data
+      end
+
+    end
+
+  end
+
+
+  # Return [high, low, open, close] for period
+  def periodCandle quotes
+    d = quotes.order(:datetime).map(&:price)
+    [d.max, d.min, d.first, d.last]
   end
 
 end
